c9816e31c65c64200697ee56171d70e1eba0cdb5
Yotic <yoticcitoy@gmail.com>
2024-07-12 10:12:10 +0300

    Fixed branches

--- Yotic.Math/Math.cs
+++ Yotic.Math/Math.cs
@@ -16,4 +16,72 @@ public static class Math
 
     public static float round(float value) => MathF.Round(value);
     public static float round(float value, int digits) => MathF.Round(value, digits);
+
+    public static double max(params double[] values)
+    {
+        if (values.Length == 0)
+            return double.NaN;
+
+        var max = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (max < values[i])
+                max = values[i];
+        return max;
+    }
+    public static float max(params float[] values)
+    {
+        if (values.Length == 0)
+            return float.NaN;
+
+        var max = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (max < values[i])
+                max = values[i];
+        return max;
+    }
+    public static int max(params int[] values)
+    {
+        if (values.Length == 0)
+            return 0;
+
+        var max = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (max < values[i])
+                max = values[i];
+        return max;
+    }
+
+    public static double min(params double[] values)
+    {
+        if (values.Length == 0)
+            return double.NaN;
+
+        var min = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (min > values[i])
+                min = values[i];
+        return min;
+    }
+    public static float min(params float[] values)
+    {
+        if (values.Length == 0)
+            return float.NaN;
+
+        var min = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (min > values[i])
+                min = values[i];
+        return min;
+    }
+    public static int min(params int[] values)
+    {
+        if (values.Length == 0)
+            return 0;
+
+        var min = values[0];
+        for (var i = 1; i < values.Length; i++)
+            if (min > values[i])
+                min = values[i];
+        return min;
+    }
 }
